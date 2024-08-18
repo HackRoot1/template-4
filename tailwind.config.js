ffcc29d224bf748bdaf57b140420cbc63dde19c9
HackRoot1 <98283417+HackRoot1@users.noreply.github.com>
2024-08-18 10:39:42 +0530

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary : "#d924ac",
+        primaryDark : "#BB2C51",
+        secondary: "#21D4B9",
+        dark: "#303030",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        cursive: ["Ephesis", "cursive"],
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '3rem',
+          xl: '4rem',
+          '2xl': '6rem',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
